b6348548dcc0dde6d66836ef2a9bdaa6aaadbb25
Sidhant Panda <sidhantpanda@gmail.com>
2016-03-03 01:04:34 +0530

    Marshmallow permissions implemented

--- app/src/main/java/com/hashexclude/simplegalleryselector/activities/ImageGalleryActivity.java
+++ app/src/main/java/com/hashexclude/simplegalleryselector/activities/ImageGalleryActivity.java
@@ -1,13 +1,19 @@
 package com.hashexclude.simplegalleryselector.activities;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.LoaderManager;
 import android.content.ClipData;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatCallback;
@@ -41,6 +47,12 @@ import java.util.List;
 public class ImageGalleryActivity extends Activity implements LoaderManager.LoaderCallbacks<List<PhotoItem>> {
 
     private static final String TAG = "ImageGalleryActivity";
+
+    private final String[] PermissionsStorage = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+    };
+
+    private final int RequestStorageId = 0;
 
     private boolean isInPreviewMode = false;
 
@@ -134,7 +146,47 @@ public class ImageGalleryActivity extends Activity implements LoaderManager.Load
         });
         // Prepare the loader.  Either re-connect with an existing one,
         // or start a new one.
-        getLoaderManager().initLoader(0, null, this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, PermissionsStorage, RequestStorageId);
+        } else {
+            getLoaderManager().initLoader(0, null, this);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case RequestStorageId:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted
+                    getLoaderManager().initLoader(0, null, this);
+                } else {
+                    AlertDialog.Builder builder =
+                            new AlertDialog.Builder(this, R.style.AppCompatAlertDialogStyle);
+                    builder.setTitle("Permission Denied");
+                    builder.setMessage("We don't have permission to access your photos. Please provide permission in order to select files from your gallery.");
+                    builder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    });
+                    builder.setPositiveButton("Grant Permission", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent();
+                            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                            Uri uri = Uri.fromParts("package", getPackageName(), null);
+                            intent.setData(uri);
+                            ImageGalleryActivity.this.startActivity(intent);
+                            dialog.dismiss();
+                        }
+                    });
+                    builder.show();
+                }
+                break;
+        }
     }
 
     @Override
